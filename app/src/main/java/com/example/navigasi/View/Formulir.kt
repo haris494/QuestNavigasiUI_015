c64ff4da83311729162fb0dbbd43301ca02aee7d
haris49 <hariis.459@gmail.com>
2025-11-07 10:05:53 +0700

    Menambahkan struktur Scaffold dengan TopAppBar pada halaman FormIsian

--- app/src/main/java/com/example/navigasi/View/Formulir.kt
+++ app/src/main/java/com/example/navigasi/View/Formulir.kt
@@ -2,7 +2,14 @@ package com.example.navigasi.View
 
 import androidx.annotation.OptIn
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.example.navigasi.R
 
 @OptIn(markerClass = ExperimentalMaterial3Api::class)
 @Composable
@@ -10,4 +17,13 @@ fun FormIsian(
     Jenis:List<String> = listOf("laki-laki","Perempuan"),
     OnSubmitBtnClick : () -> Unit
 ){
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.home), color = Color.White) },
+
+        }
+
+
+
 
