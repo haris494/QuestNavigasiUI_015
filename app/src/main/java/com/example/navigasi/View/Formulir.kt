b2caf9c3de0c48b8c757e872b87b2f618d5ab527
haris49 <hariis.459@gmail.com>
2025-11-07 10:10:16 +0700

    Menambahkan warna latar pada TopAppBar menggunakan TopAppBarDefaults

--- app/src/main/java/com/example/navigasi/View/Formulir.kt
+++ app/src/main/java/com/example/navigasi/View/Formulir.kt
@@ -5,9 +5,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.navigasi.R
 
@@ -21,7 +23,8 @@ fun FormIsian(
         topBar = {
             TopAppBar(
                 title = { Text(text = stringResource(id = R.string.home), color = Color.White) },
-
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700))
         }
 
 
