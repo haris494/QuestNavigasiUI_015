52a25dca9861b470b9bc6be157f6cd182dac5c0f
haris49 <hariis.459@gmail.com>
2025-11-07 10:22:52 +0700

    Menambahkan komponen OutlinedTextField sebagai input teks tunggal

--- app/src/main/java/com/example/navigasi/View/Formulir.kt
+++ app/src/main/java/com/example/navigasi/View/Formulir.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -34,4 +35,7 @@ fun FormIsian(
         Column (modifier = Modifier.padding(paddingValues = isiRuang),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally){
-
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
