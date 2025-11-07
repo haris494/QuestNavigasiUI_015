46a5393b88b63254435bfb04699a3e8d431db902
haris49 <hariis.459@gmail.com>
2025-11-07 10:24:58 +0700

    Menambahkan modifier padding dan lebar pada OutlinedTextField

--- app/src/main/java/com/example/navigasi/View/Formulir.kt
+++ app/src/main/java/com/example/navigasi/View/Formulir.kt
@@ -4,6 +4,7 @@ import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -16,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.navigasi.R
 
 @OptIn(markerClass = ExperimentalMaterial3Api::class)
@@ -39,3 +41,5 @@ fun FormIsian(
                 value = "",
                 singleLine = true,
                 modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
