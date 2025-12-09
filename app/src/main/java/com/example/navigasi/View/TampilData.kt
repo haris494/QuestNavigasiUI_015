e4e7abcea34f8df212b5ce1d8fe0e3183a7e4598
haris49 <hariis.459@gmail.com>
2025-12-09 17:42:55 +0700

    add TampilData composable with Material3 opt-in

---
+++ app/src/main/java/com/example/navigasi/View/TampilData.kt
@@ -0,0 +1,10 @@
+package com.example.navigasi.View
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnClick: () -> Unit
+) {
